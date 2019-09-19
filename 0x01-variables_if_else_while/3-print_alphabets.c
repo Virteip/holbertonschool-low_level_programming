f952d7c35b73c84657bd479a14b8245edae6cc77
Sergio <sergiopietri@gmail.com>
2019-09-19 17:48:05 +0000

    used for

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(toupper(letter));
+putchar('\n');
+return (0);
+}
+
