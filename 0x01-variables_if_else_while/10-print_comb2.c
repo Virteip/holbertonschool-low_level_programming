59c4c53ae4081d3c8e680bfcaef78ca10ed54a26
Sergio <sergiopietri@gmail.com>
2019-09-20 00:58:10 +0000

    YISUS

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,29 +8,21 @@
  */
 int main(void)
 {
-	int n;
-	int l;
-
-	for (n = 48; n <= 57; n++)
-	{
-
-		for (l = 48; l <= 57; l++)
-		{
-			putchar(n);
-                        putchar(l);	
-			if(n != '9' || l != '9'){	
-                        putchar(',');
-                        putchar(' ');
-                                            }	
-		}
-
-
-
-
-
-		
-		}
-
-	putchar('\n');
-	return (0);
+int n;
+int l;
+for (n = 48; n <= 57; n++)
+{
+for (l = 48; l <= 57; l++)
+{
+putchar(n);
+putchar(l);
+if (n != '9' || l != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
