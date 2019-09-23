a359c1f1cc939ba0a3c6c471eb8fcbb18d458463
Sergio <sergiopietri@gmail.com>
2019-09-23 23:48:17 +0000

    prints 8044

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+int print_last_digit(int n)
+{
+	
+	int l = n % 10;
+	
+	if (l < 0)
+	{
+	l = l * -1;	
+	_putchar(l + '0');
+	} 
+        else
+	{
+	_putchar(l + '0');	
+	}
+	
+
+	return(l);
+}
