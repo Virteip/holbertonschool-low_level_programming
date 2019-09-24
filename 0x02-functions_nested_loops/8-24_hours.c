b98bd3302bf5f39a725d64f22e2c19dea23f1922
Sergio <sergiopietri@gmail.com>
2019-09-24 14:56:07 +0000

    jack and betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,19 +7,18 @@
 **/
 void jack_bauer(void)
 {
-int c,d;	
-	for (c = 0; c <= 24; c++)
-	{
-		for (d = 0; d <= 60; d++)
-		{
-			_putchar((c/10)+'0');
-			_putchar((c%10)+'0');
-			_putchar(':');
-			_putchar((d/10)+'0');
-			_putchar((d%10)+'0');
-			_putchar('\n');
+int c,d;
 
+	for (c = 0; c <= 23; c++)
+	{
+		for (d = 0; d <= 59; d++)
+		{
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+			_putchar(':');
+			_putchar((d / 10) + '0');
+			_putchar((d % 10) + '0');
+			_putchar('\n');
 		}
 	}
-		
 }
