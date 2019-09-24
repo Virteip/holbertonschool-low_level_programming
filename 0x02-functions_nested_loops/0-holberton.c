23b8c5d2e93b3b5cfda9ee65a4144c1cf4c5af00
Sergio <sergiopietri@gmail.com>
2019-09-24 13:18:20 +0000

    fixed betty

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
-
-int main (void)
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
 	int i;
-	char hol[9]="Holberton";
-	for(i = 0 ;i <= 9 ; i++ )
-	{
-	_putchar(hol[i]);
-	}
-	_putchar('\n');
-	return(0);
+	char hol[9] = "Holberton";
 
+	for (i = 0 ; i <= 9 ; i++)
+	{
+	_putchar (hol[i]);
+	}
+	_putchar ('\n');
+	return (0);
 }
