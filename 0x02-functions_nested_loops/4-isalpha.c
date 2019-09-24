ffca9b4562e302e1685d84d163230780dac6385c
Sergio <sergiopietri@gmail.com>
2019-09-24 14:00:40 +0000

    fixed betty

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
-
+/**
+* _isalpha - check the code for Holberton School students.
+*@c: integer
+* Return: Always.
+*/
 int _isalpha(int c)
 {
-	if ( (c > 65 && c < 90) || (c > 97 && c < 122))
-	{
-		return(1);
-	} else {
-		return(0);
-	}
+	if ((c > 65 && c < 90) || (c > 97 && c < 122))
+		return (1);
+	else
+		return (0);
 
 }
