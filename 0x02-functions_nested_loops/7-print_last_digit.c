32ef00d933d8a37f27f3935715db9aa7b7ba552d
Sergio <sergiopietri@gmail.com>
2019-09-24 14:36:55 +0000

    fixed betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
-
+/**
+* print_last_digit - check the code for Holberton School students.
+*@n: integer.
+* Return: Always.
+*/
 int print_last_digit(int n)
 {
-	
 	int l = n % 10;
-	
+
 	if (l < 0)
 	{
-	l = l * -1;	
-	_putchar(l + '0');
-	} 
-        else
-	{
-	_putchar(l + '0');	
+	l = l * -1;
+	_putchar (l + '0');
 	}
-	
-
-	return(l);
+	else
+	{
+	_putchar(l + '0');
+	}
+	return (l);
 }
