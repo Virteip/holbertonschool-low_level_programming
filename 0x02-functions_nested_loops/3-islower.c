0fc1ee1202af506b1604dfa4d4a5475d5837baf8
Sergio <sergiopietri@gmail.com>
2019-09-24 13:57:37 +0000

    fixed betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
-
+/**
+* _islower - check the code for Holberton School students.
+*@c: ineteger.
+* Return: Always.
+*/
 int _islower(int c)
 {
 	if (c < 97)
-	{
-		return(0);
-	} else {
-		return(1);
-	}
+		return (0);
+	else
+		return (1);
 
 }
