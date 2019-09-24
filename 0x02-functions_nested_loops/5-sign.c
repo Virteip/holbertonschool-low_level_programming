aa751a4c8fa61fd23d39b7abcb183e6b43702a47
Sergio <sergiopietri@gmail.com>
2019-09-24 14:20:13 +0000

    betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,23 @@
 #include "holberton.h"
-
+/**
+* print_sign - check the code for Holberton School students.
+*@n: integer.
+* Return: Always.
+*/
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return(1);
-	} else if (n == 0 )
+		_putchar ('+');
+		return (1);
+	} else if (n == 0)
 	{
-		_putchar('0');
-		return(0);
-	} else 
-	{
-		_putchar('-');
-		return(-1);
+		_putchar ('0');
+		return (0);
 	}
-
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
 }
