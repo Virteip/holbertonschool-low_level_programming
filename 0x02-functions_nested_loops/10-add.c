dd54317ceec120d2788049ded9c67ad7a7ea03b7
Sergio <sergiopietri@gmail.com>
2019-09-24 17:07:43 +0000

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* times_table - check the code for Holberton School students.
+*
+* Return: Always.
+*/
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	
+	return(sum);
+	_putchar ('\n');
+}
