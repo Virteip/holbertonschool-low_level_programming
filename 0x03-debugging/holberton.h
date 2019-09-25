35b994b2660cdc7848b88eddaf5abed951bcfe31
Sergio <sergiopietri@gmail.com>
2019-09-25 02:57:14 +0000

    fixing holberton 5

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,10 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdio.h>
-void positive_or_negative(int i);
-#endif /* HOLBERTON_H */
+#endif
 
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+void positive_or_negative(int i);
+#endif /* HOLBERTON_H */
+
+#ifndef LARGEST_H
+#define LARGEST_H
 int largest_number(int a,int b,int c);
 #endif /*HOLBERTON*/
