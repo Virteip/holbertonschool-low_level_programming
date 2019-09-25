f0b1e08a2ab23782a29e45f1bad2f33d2222b2cb
virteip <sergiopietri@gmail.com>
2019-09-25 22:27:17 +0000

    fixed betty on 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
 void print_times_table(int n)
 {
 	int a, b, mult;
-		
+
 if (n < 15 && n >= 0)
 {
 	for (a = 0; a <= n; a++)
@@ -24,7 +24,7 @@ if (n < 15 && n >= 0)
 				_putchar (' ');
 				_putchar (' ');
 				_putchar (' ');	}
-			_putchar ((mult % 10)+'0');	}
+			_putchar ((mult % 10) + '0');	}
 			else if (mult >= 10 && mult <= 99)
 			{
 				if (b != 0)
@@ -41,7 +41,7 @@ if (n < 15 && n >= 0)
 				_putchar (',');
 				_putchar (' ');	}
 			_putchar ((mult / 100) + '0');
-			_putchar (((mult / 10)%10) + '0');
+			_putchar (((mult / 10) % 10) + '0');
 			_putchar ((mult % 10) + '0');	}	}
 _putchar ('\n');	}	}
 
