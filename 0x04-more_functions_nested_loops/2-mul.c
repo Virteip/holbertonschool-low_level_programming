632ca949d2e8a5a1d2b1f3085078d60006e21a51
virteip <sergiopietri@gmail.com>
2019-09-26 15:57:01 +0000

    exercise #2 without betty

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* mul - check the code for Holberton School students.
+*
+* Return: Always.
+*/
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
