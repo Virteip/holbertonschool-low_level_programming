125236fc285948cce2fc41ed99c463c499f3d0a9
virteip <sergiopietri@gmail.com>
2019-09-26 15:24:30 +0000

    add #1 without betty

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _islower - check the code for Holberton School students.
+*@c: ineteger.
+* Return: Always.
+*/
+int _isdigit(int c)
+{
+	if (c <= 57 && c >= 48)
+		return (1);
+	else
+		return (0);
+
+}
