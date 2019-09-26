22b82d734a99b24b9af599362ca7064174760d45
virteip <sergiopietri@gmail.com>
2019-09-26 19:57:43 +0000

    fixed amount of putchars

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,17 +11,13 @@ void more_numbers(void)
 
 while (x < 10)
 {
-	for (i = 1; i <= 14; i++)
+	for (i = 0; i <= 14; i++)
 	{
-		if (i < 10)
-		{
-		_putchar (i + '0');
-		}
-		else if (i >= 10)
+		if (i >= 10)
 		{
 		_putchar ((i / 10) + '0');
-		_putchar ((i % 10) + '0' '\n');
 		}
+		_putchar ((i % 10) + '0');
 	}
 	x++;
 	_putchar('\n');
