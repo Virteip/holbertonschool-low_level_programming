4260dd01d25736ef5e78aace78a2bc8eaaaa399d
virteip <sergiopietri@gmail.com>
2019-09-27 18:39:25 +0000

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int divisor = 2;
+long int numero = 612852475143;
+long factor = numero;
+
+while (factor > 1)
+{
+	if ((factor % a) == 0)
+	{
+		factor /= divisor;
+	}
+	else
+	{
+		divisor++;
+	}
+
+}
+	printf("%d\n", divisor);
+	return (0);
+}
