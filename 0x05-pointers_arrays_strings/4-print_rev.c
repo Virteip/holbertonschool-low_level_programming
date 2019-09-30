d7a93d99f48f530e3437066184ac3127f8876004
virteip <sergiopietri@gmail.com>
2019-09-30 21:33:35 +0000

    reverse without betty

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+  * modif_my_param - set the integer to 402
+  * @s: a pointer the integer we want to set to 402
+  *
+  * Return: int
+  */
+void print_rev(char *s)
+{
+	int arrayNum;
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+	s++;
+	i++;
+	}
+
+	for (arrayNum = i; arrayNum >= 0; arrayNum--)
+	{
+	_putchar(*s);
+	s--;
+	}
+
+
+	_putchar('\n');
+}
