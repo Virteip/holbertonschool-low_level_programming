8ff4c937ec3d9ae0ba5561310393dbc7021a56b5
virteip <sergiopietri@gmail.com>
2019-09-30 15:34:51 +0000

    initializing project folder

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* modif_my_param - set the integer to 402
+* @m: a pointer the integer we want to set to 402
+*
+* Return: nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
