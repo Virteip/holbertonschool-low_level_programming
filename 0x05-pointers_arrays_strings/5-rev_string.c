373793903141696767cbf9929ff38b7afffbf9f4
virteip <sergiopietri@gmail.com>
2019-10-01 15:51:14 +0000

    removed stdio

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
-  * rev_string - set the integer to 402
-  * @s: a pointer the integer we want to set to 402
-  *
-  * Return: int
-  */
+* rev_string - set the integer to 402
+* @s: a pointer the integer we want to set to 402
+*
+* Return: int
+*/
 void rev_string(char *s)
 {
-int i, g, h;
+	int i, g, h;
 
-g = sizeof(s);
+	g = sizeof(s);
 
-for (i = 0; i <= g; i++)
-{
+	for (i = 0; i <= g; i++)
+	{
 	h = s[i];
 	s[i] = s[g];
 	s[g] = h;
 	g--;
-}
+	}
 
 }
-
