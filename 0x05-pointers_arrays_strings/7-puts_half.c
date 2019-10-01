2f7db0df17b0e9a5e21a93220c958c906fa917b2
virteip <sergiopietri@gmail.com>
2019-10-01 22:48:28 +0000

    adding with betty #7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,23 +1,23 @@
 #include "holberton.h"
 /**
-* puts2 - set the integer to 402
+* puts_half - set the integer to 402
 * @str: a pointer the integer we want to set to 402
 *
 * Return: int
 */
 void puts_half(char *str)
 {
-	int g, halfmod, half, n, length_of_the_string;
+	int g, halfmod, half, n;
 
 	g = 0;
 	while (str[g] != '\0')
 	{
 		g++;
 	}
-	
+
 	half = g / 2;
 	halfmod = g % 2;
-	length_of_the_string = g;
+
 
 	if (halfmod == 0)
 	{
@@ -29,14 +29,12 @@ void puts_half(char *str)
 	}
 	else
 	{
-		n = (length_of_the_string - 1) / 2;
-		n = n + 1;
-		length_of_the_string = length_of_the_string - 1;
-		while (n <= length_of_the_string)
+		n = (g - 1) / 2;
+		n++;
+		while (n <= g)
 		{
 		_putchar(str[n]);
 		n++;
-
 		}
 	}
 
