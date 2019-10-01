5c7c29a9cb52ce1d347e9db603055899e1bd6d97
virteip <sergiopietri@gmail.com>
2019-10-01 14:25:23 +0000

    fixed betty on 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
-* modif_my_param - set the integer to 402
-* @m: a pointer the integer we want to set to 402
-*
+* swap_int - set the integer to 402
+* @a: a pointer the integer we want to set to 402
+* @b: b pointer to int.
 * Return: nothing
 */
 void swap_int(int *a, int *b)
-{	
+{
 	int c = *a;
 	int d = *b;
 
