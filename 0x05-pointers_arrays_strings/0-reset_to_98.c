e5d60453fcd97d58d9d7ded852460a2b7f83f1b0
virteip <sergiopietri@gmail.com>
2019-10-01 14:13:41 +0000

    fixed betty on 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
-* modif_my_param - set the integer to 402
-* @m: a pointer the integer we want to set to 402
+* reset_to_98 - set the integer to 402
+* @n: a pointer the integer we want to set to 402
 *
 * Return: nothing
 */
