9714cc3ef22690def382f1ec6d4678d654173ce8
virteip <sergiopietri@gmail.com>
2019-10-01 15:36:45 +0000

    adding #5 reverse print

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
-  * modif_my_param - set the integer to 402
+  * rev_string - set the integer to 402
   * @s: a pointer the integer we want to set to 402
   *
   * Return: int
   */
 void rev_string(char *s)
 {
-/*int i, g;
+int i, g, h;
 
-g = 0;
-for(i = sizeof(s); i >= 0; i--)
+g = sizeof(s);
+
+for (i = 0; i <= g; i++)
 {
-	s[g] = s[i];
-	g++;
-}*/
-int g=0;
-while (g != sizeof(s))
-{
-int i = sizeof(s);
-	s[g]=s[i];
-	g++;
-	i--;
+	h = s[i];
+	s[i] = s[g];
+	s[g] = h;
+	g--;
 }
+
 }
 
