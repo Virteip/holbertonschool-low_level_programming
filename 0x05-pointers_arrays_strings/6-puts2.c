eb0a01789a2504daf01491ee293c1c535e18d4f5
virteip <sergiopietri@gmail.com>
2019-10-01 20:21:09 +0000

    even this too

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,10 +14,16 @@ void puts2(char *str)
 	{
 		g++;
 	}
+	
 	b = 0;
-	while (b <= g)
+	while (b != g)
 	{
+	int mod = b%2;
+		if (mod == 0)
+		{
 		_putchar(str[b]);
-		b = b + 2;
+		}
+	b = b + 2;
 	}
+	_putchar('\n');
 }
