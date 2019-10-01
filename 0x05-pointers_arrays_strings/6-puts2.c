6fb504b1e7b881e7b74c3a77f511f30e6cbf1ff8
virteip <sergiopietri@gmail.com>
2019-10-01 19:45:43 +0000

    even this

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* puts2 - set the integer to 402
+* @str: a pointer the integer we want to set to 402
+*
+* Return: int
+*/
+void puts2(char *str)
+{
+	int g, b;
+
+	g = 0;
+	while (str[g] != '\0')
+	{
+		g++;
+	}
+	b = 0;
+	while (b <= g)
+	{
+		_putchar(str[b]);
+		b = b + 2;
+	}
+
+_putchar('\n');
+}
