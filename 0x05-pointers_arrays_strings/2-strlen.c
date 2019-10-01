8d60993a3c04bb042b5a7a9656f48b48dbf8d5e6
virteip <sergiopietri@gmail.com>
2019-10-01 14:27:28 +0000

    fixed betty on 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* modif_my_param - set the integer to 402
+*_strlen  - set the integer to 402
 * @s: a pointer the integer we want to set to 402
 *
 * Return: int
@@ -8,7 +8,8 @@
 int _strlen(char *s)
 {
 	int i = 0;
-	while (*s != '\0') 
+
+	while (*s != '\0')
 	{
 	s++;
 	i++;
