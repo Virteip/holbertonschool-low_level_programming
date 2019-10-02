9ef633af4dcb7da30689ee7f99e144e85976fd1c
virteip <virteip@github.com>
2019-10-02 04:01:45 +0000

    8 with betty and god commas

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,26 +1,26 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-* puts_half - set the integer to 402
-* @str: a pointer the integer we want to set to 402
-*
-* Return: int
+* print_array - set the integer to 402
+* @a: a pointer the integer we want to set to 402
+* @n: integer
+* Return: void
 */
 void print_array(int *a, int n)
 {
 	int s;
-	
+
 	s = 0;
 	n = n - 1;
 	while (s <= n)
 	{
 		if (s != n)
 		{
-		printf("%d ,",a[s]);
+		printf("%d, ", a[s]);
 		}
 		else
 		{
-		printf("%d",a[s]);
+		printf("%d", a[s]);
 		}
 	s++;
 	}
