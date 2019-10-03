a8267de63f2bddf32b4bc1451a4f9971dc958cf8
virteip <sergiopietri@gmail.com>
2019-10-03 19:48:22 +0000

    exercise #3 with betty

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strcmp - count the characters on a string
+ * @s1: a pointer the integer we want to set to 402
+ * @s2: pointer
+ *
+ * Return: char
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int pos;
+	int res;
+
+	for (pos = 0; pos != '\0'; pos++)
+	{
+		s2 += s1[pos];
+		s1 += s2[pos];
+	}
+
+	res = *s1 - *s2;
+
+	return (res);
+}
