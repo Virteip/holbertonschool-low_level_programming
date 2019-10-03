e79132349f62a89e177435e92cf34d8e86161bf0
virteip <sergiopietri@gmail.com>
2019-10-03 22:33:28 +0000

    exercise 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * string_toupper - count the characters on a string
+ * @char*: a pointer the integer we want to set to 402
+ *
+ *
+ * Return: char
+ */
+char *string_toupper(char *c)
+{
+	int length = 0;
+	int a;
+
+	while (c[length] != '\0')
+	{
+		length++;
+	}
+
+	for (a = 0; a < length; a++)
+	{
+		if (c[a] >= 97 && c[a] <= 122)
+		{
+		c[a] = c[a] - 32;
+		}
+	}
+
+	return (c);
+}
