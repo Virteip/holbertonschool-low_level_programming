09e3ba79d7f595d4748a2b827870f3dd31939ac7
virteip <sergiopietri@gmail.com>
2019-10-07 16:16:39 +0000

    1 and 0 with betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
+ * _memset - prints buffer in hexa
+ * @s: the address of memory to print
+ * @b: the size of the memory to print
+ * @n: integer
  * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	char* p = (char*)s;
-	for (i = 0; i < n; ++i) 
+	char *p =  s;
+
+	for (i = 0; i < n; ++i)
 	{
 		p[i] = b;
 	}
