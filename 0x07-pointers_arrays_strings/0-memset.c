46c0e7bd489fb3456b3f28a71a717495d9eeee64
virteip <sergiopietri@gmail.com>
2019-10-07 15:47:16 +0000

    initialize project

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	char* p = (char*)s;
+	for (i = 0; i < n; ++i) 
+	{
+		p[i] = b;
+	}
+	return (s);
+}
