73dfbeccb79c3447e52e75a59823e8f923158faa
virteip <sergiopietri@gmail.com>
2019-10-07 19:13:49 +0000

    number 2 (1)

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strchr - prints buffer in hexa
+ * @c: the address of memory to print
+ * @s: the size of the memory to print
+ *
+ *
+ * Return: Nothing.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *p = s;
+
+	for (i = 0; i != c; ++i)
+	{
+		if (p[i] == c)
+		{
+			while (p[i] != '\0')
+			{
+				s = &p[i];
+				i++;
+				return (s);
+			}
+			break;
+		}
+	}
+	return (s);
+}
