b356a28b67b9f72fbf0e8204742052bf03a113b3
virteip <sergiopietri@gmail.com>
2019-10-08 00:13:47 +0000

    number 4 (0)

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _strpbrk - prints buffer in hexa
+ * @accept: the address of memory to print
+ * @s: the size of the memory to print
+ *
+ *
+ * Return: Nothing.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p = s;
+
+	for (i = 0; i <= '0'; ++i)
+	{
+		for (j = 0; j <= '0'; j++)
+		{
+			if (p[i] == accept[j])
+			{
+				s = &p[i];
+				i++;
+				return (s);
+			}
+		}
+	}
+	return ('\0');
+}
