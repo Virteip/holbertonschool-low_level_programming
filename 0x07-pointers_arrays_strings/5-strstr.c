30aef19694754151bed78258d4773645249d5d39
virteip <sergiopietri@gmail.com>
2019-10-08 02:27:22 +0000

    number 5 (0)

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strstr - The name of the function.
+ * @haystack:  var char
+ * @needle: var char
+ * Return: n.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*haystack == *n)
+		{
+			haystack++;
+			n++;
+		}
+		if (!*n)
+			return (h);
+
+		haystack = h + 1;
+	}
+	return ('\0');
+}
