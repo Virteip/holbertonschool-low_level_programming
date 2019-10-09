cd4ff9161d5a0fcd5b3e580aadf6f2c18cb6c793
virteip <sergiopietri@gmail.com>
2019-10-09 19:04:53 +0000

    #3 (0)

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * factorial - check the code for Holberton School students.
+ *@n: reverse.
+ * Return: factorial.
+ */
+int factorial(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+		return (n * factorial(n - 1));
+}
