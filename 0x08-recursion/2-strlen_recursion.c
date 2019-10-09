2d82b00dabebd3ee306abc30b32f4ab359cae6ff
virteip <sergiopietri@gmail.com>
2019-10-09 18:38:27 +0000

    #2 (0)

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ *@s: reverse.
+ * Return: N\a.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
