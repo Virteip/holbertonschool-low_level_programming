8f29a3ba93a3803af2d502accb53a81ecb611b65
virteip <sergiopietri@gmail.com>
2019-10-11 18:48:45 +0000

    #1 (0)

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints buffer in hexa
+ * @argc: argument count
+ * @argv: argument value (void)
+ *
+ *
+ * Return: Nothing.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%i \n", argc);
+
+	return (0);
+}
