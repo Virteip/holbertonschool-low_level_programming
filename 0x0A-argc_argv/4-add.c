c3c71e278acf1a2db4126af6d9ab872d8a091485
virteip <sergiopietri@gmail.com>
2019-10-11 23:48:35 +0000

    #4 (2)

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-int check(char *s)
+/**
+ * main - prints buffer in hexa
+ * @argc: argument count
+ * @argv: argument value
+ *
+ *
+ * Return: Nothing.
+ */
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i, r;
 
-	for(i = 0; s[i] != '\0'; i++)
+	if (argc == 0)
 	{
-		if(s[i] < 48 || s[i] > 97)
-		{
-			return (1);
-		}
+		printf("0 \n");
+		return (1);
 	}
-	return(0);
-}
-
-int main (int argc, char *argv[])
-{
-	int i,r;
-
-	for (i = 0; i < argc; i++)
+	else
 	{
-		if (check(*argv) == 1)
+		for (i = 1; i < argc; i++)
 		{
-			for (i = 1; i < argc; i++)
+			if (*argv[i] > 48 && *argv[i] < 57)
 			{
 				r += atoi(argv[i]);
 			}
+			else
+			{
+				puts("Error");
+				return (1);
+			}
 		}
-		else if (check(*argv) == 0)
-		{
-			puts("Error");
-			return (1);
-		}
+		printf("%i \n", r);
 	}
-		printf("%i \n",r);
-		return (r);
+	return (0);
 }
