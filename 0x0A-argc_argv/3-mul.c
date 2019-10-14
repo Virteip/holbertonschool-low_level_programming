84ba4aa3e32534f8b9539a2499ad1634bf5e5ec3
virteip <sergiopietri@gmail.com>
2019-10-14 19:23:34 +0000

    pld correction on 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,15 @@ int main(int argc, char *argv[])
 {
 	int res;
 
-	if (argc - 1 > 1)
+	if (argc == 3)
 	{
 		res = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d \n", res);
-		return (0);
 	}
-
-	puts("Error");
-	return (1);
+	else
+	{
+		puts("Error");
+		return (1);
+	}
+	return (0);
 }
