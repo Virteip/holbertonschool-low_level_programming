e6845964c1508ad238da6e2cbe9a23a0b5009a22
virteip <sergiopietri@gmail.com>
2019-10-15 17:38:30 +0000

    #0 (0)

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - prints buffer in hexa
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i = 0;
+
+	if (size != 0)
+	{
+		array = malloc(sizeof(char) * size);
+
+		while (i < size)
+		{
+			array[i] = c;
+			i++;
+
+		}
+
+		return (array);
+	}
+
+	return (NULL);
+
+}
