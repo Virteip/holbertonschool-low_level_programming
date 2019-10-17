36af9b491864a58f8e2bf42f8a0fe7d7111c73db
virteip <sergiopietri@gmail.com>
2019-10-17 20:09:23 +0000

    #3 (0)

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - count the characters on a string
+ * @min: char
+ * @max: char
+ * Return: char
+ */
+int *array_range(int min, int max)
+{
+	int i, j;
+	int *array;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	j = max - min;
+	array = malloc(sizeof(int) * (j) + 1);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= max; min++, i++)
+	{
+		array[i] = min;
+	}
+	return (array);
+}
