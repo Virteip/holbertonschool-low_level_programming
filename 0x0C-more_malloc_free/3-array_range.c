56648c80a905267d1088caa6314baf303674aa23
virteip <sergiopietri@gmail.com>
2019-10-17 20:37:17 +0000

    #3 (2)

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,18 +18,17 @@ int *array_range(int min, int max)
 	}
 
 	j = max - min;
-	array = malloc(sizeof(int) * (j) + 1);
+	array = malloc(sizeof(int) * (j + 1));
 
 	if (array == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i <= max; min++, i++)
+	for (i = 0; i <= j; i++, min++)
 	{
 		array[i] = min;
 	}
 
-	array[i] = '\0';
 	return (array);
 }
