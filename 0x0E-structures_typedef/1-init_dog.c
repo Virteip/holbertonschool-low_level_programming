c0d64e0d8a242d24de9533f4b9018f6163e6ca6f
virteip <sergiopietri@gmail.com>
2019-10-21 19:25:56 +0000

    #1 (0)

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,15 +1,17 @@
 #include "dog.h"
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
+ * init_dog - check the code for Holberton School students.
+ *@d: d
+ *@name: name
+ *@age: age
+ *@owner: owner
  *
  * Return: Always 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
-
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
