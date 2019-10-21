64e452afd2b8b029200d7fddbe97e6cbc9160e92
virteip <sergiopietri@gmail.com>
2019-10-21 20:48:10 +0000

    #4 (2)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,14 +12,12 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *d;
 
 	d = malloc(sizeof(struct dog));
 
 	if (d == NULL)
 		return (NULL);
-	x = name;
-	y = owner;
 
 	d->name = name;
 	d->age = age;
