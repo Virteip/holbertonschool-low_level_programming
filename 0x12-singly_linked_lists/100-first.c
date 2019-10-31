240e7e4f0078555ba25a5c5d4dfb41774d9478df
virteip <sergiopietri@gmail.com>
2019-10-31 18:29:04 -0500

    [Feature] #5 (0)

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * myCleanupFun - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void myCleanupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my
+back!\n");
+}
