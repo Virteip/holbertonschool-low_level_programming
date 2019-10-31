d568a259d0eea3df7570d24154b98dd16538c0aa
virteip <sergiopietri@gmail.com>
2019-10-31 18:53:44 -0500

    [Feature] #5 (1)

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * myCleanupFun - check the code for Holberton School students.
+ * Constructor - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-void myCleanupFun(void)
+void main (void) __attribute__ ((constructor));
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my
 back!\n");
