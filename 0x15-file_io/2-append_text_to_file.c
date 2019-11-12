2af55c71b9da44f80653f2e8e4bfe3ba3a124f2b
virteip <sergiopietri@gmail.com>
2019-11-12 18:23:12 -0500

    [Feat]: 2 * 0

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+
+/**
+ *_strlen  - set the integer to 402
+ * @s: a pointer the integer we want to set to 402
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * append_text_to_file - check the code for Holberton School students.
+ * @filename: str
+ * @text_content: byte n
+ *
+ * Return: Always 0.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op, wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	op = open(filename, O_APPEND | O_WRONLY, 0600);
+
+	if (op == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		return (close(op) == -1 ? -1 : 1);
+	}
+
+	wr = write(op, text_content, _strlen(text_content));
+
+	if (wr == -1)
+	{
+		return (-1);
+	}
+
+	close(op);
+
+	return (1);
+}
