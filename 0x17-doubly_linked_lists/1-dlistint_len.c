0c454ebb747704642a2b21fce8943b0b7be0b7f4
Virteip <sergiopietri@gmail.com>
2019-12-13 14:20:41 +0000

    [lists.h && #1]

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - print double linked list length.
+ * @h: pointer to list.
+ * Return: int.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
