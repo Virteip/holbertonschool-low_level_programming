47486b301a8e346f87a73942956c9f96a66158c0
Virteip <sergiopietri@gmail.com>
2020-01-18 12:58:30 -0500

    [#0 (7)]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	/*reserve space for hash table*/
-	hash_table_t *hash_table = malloc(sizeof(hash_table_t *) * size);
-	/*if malloc fails return NULL*/
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	/*if malloc fails return NULL and free table*/
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
 	/*set size and array nodes space*/
 	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_node_t *));
+	hash_table->array = calloc(size, sizeof(hash_node_t *) * 0);
+
+	if (!(hash_table->array))
+	{
+		free(hash_table);
+		return (NULL);
+	}
 
 	return (hash_table);
 }
