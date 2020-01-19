8e48b826909a3bbbbeb009dcd4abeb1f38d3e309
Virteip <sergiopietri@gmail.com>
2020-01-18 22:10:09 -0500

    [#0 (12)]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,21 +8,26 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
 
-	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (hash_table->array == NULL)
 	{
 		return (NULL);
 	}
+
+	hash_table->size = size;
 
 	return (hash_table);
 }
