a4b09b2df365bdae81ebdf42cba03d34ae746506
Virteip <sergiopietri@gmail.com>
2020-01-21 07:44:50 -0500

    [#9 3]

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,36 +2,35 @@
 /**
  * insert_nodeint_at_index - add int at the end
  * @head: list
- * @indx: int to print if found
+ * @idx: int to print if found
  * @n: int
  * Return: int
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node;
-	listint_t *tmp;
-	/*listint_t *savehead;*/
-	unsigned int number;
+	listint_t *temp, *copy_head;
+	unsigned long int f;
 
-	tmp = *head;
-	node = malloc(sizeof(listint_t));
-	if (node == NULL)
-		return (NULL);
-
-	if (*head == NULL)
-		node = *head;
-	else
+	copy_head = *head;
+	if (idx != 0)
 	{
-		node->n = n;
-		number = 0;
-		while (number < idx - 1)
-		{
-			tmp = tmp->next;
-	        	number++;
-		}
-		node->next = tmp->next;
-		tmp->next = node;
-	}
+		for (f = 0; f < idx - 1 && copy_head != NULL; i++)
+			copy_head = copy_head->next; }
+	if (copy_head == NULL && idx != 0)
+	{
+		return (NULL); }
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		return (NULL); }
+	temp->n = n;
+	if (idx == 0)
+	{
+		temp->next = *head;
+		*head = temp; }
+	else if (idx != 0)
+	{
+		temp->next = copy_head->next;
+		copy_head->next = temp; }
 
-	return (*head);
-}
+	return (temp); }
