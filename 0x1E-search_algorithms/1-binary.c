bb31a0e92571185028c3e277dc8d5f616c887187
Virteip <sergiopietri@gmail.com>
2020-04-14 14:06:13 -0500

    [Update] T#1 add print

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,11 +13,20 @@ int binary_search(int *array, size_t size, int value)
 	size_t low = 0;
 	size_t high = size - 1;
 	size_t mid;
+	size_t i;
 
 	if (array)
 	{
 		while (low <= high)
 		{
+			printf("Searching in array: ");
+			for (i = low; i < high; i++)
+			{
+				printf("%d, ", array[i]);
+			}
+			printf("%d", array[i]);
+			printf("\n");
+
 			mid = low + (high - low) / 2;
 
 			if (array[mid] == value)
